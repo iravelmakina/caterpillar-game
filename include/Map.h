db615a048de482fc937afc08d79f0ab36b67030e
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2024-10-31 15:58:48 +0200

    Map class
    - implemented Map class with size, walls and grid fields
    - created methods withinBounds(), isWall(), addWall() and generateRandomWallBlocks() for generating walls of different types
    - created helper methods generateRandomWallBlock() and isBlockOverlap()
    -  created enum class BlockType for storing possible types of wall blocks

---
+++ include/Map.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <vector>
+
+
+enum class BlockType {
+    SINGLE,
+    HORIZONTAL,
+    VERTICAL,
+    L_SHAPE
+};
+
+class Map {
+public:
+    Map(unsigned int width, unsigned int height, unsigned int blockCount);
+
+    bool withinBounds(const std::pair<unsigned int, unsigned int> &position) const;
+
+    bool isWall(const std::pair<unsigned int, unsigned int> &position) const;
+
+    void addWall(const std::pair<unsigned int, unsigned int> &position);
+
+    void generateRandomWallBlocks(unsigned int blockCount);
+
+private:
+    std::pair<unsigned int, unsigned int> size;
+    std::vector<std::pair<unsigned int, unsigned int> > walls;
+    std::vector<std::vector<char> > grid;
+
+    std::vector<std::pair<unsigned int, unsigned int>> generateRandomWallBlock() const;
+    bool isBlockOverlap(const std::vector<std::pair<unsigned int, unsigned>> &block) const;
+};
