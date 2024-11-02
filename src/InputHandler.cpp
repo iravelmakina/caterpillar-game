ea525470b47ff02afa766d8fbf2a0dd7d8aceba2
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2024-11-02 13:49:23 +0200

    InputHandler class
    - implemented InputHandler class
    - created method handleInput() with helper methods handleStartMenuInput(), handlePlayingInput(), handlePausedInput(), handleEndMenuInput()

---
+++ src/InputHandler.cpp
@@ -0,0 +1,68 @@
+#include "InputHandler.h"
+
+
+void InputHandler::handleInput(SnakeGame &game, sf::RenderWindow &window) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+        if (event.type == sf::Event::Closed) {
+            window.close();
+        }
+
+        switch (game.getState()) {
+            case State::START_MENU:
+                handleStartMenuInput(game, event);
+                break;
+            case State::PLAYING:
+                handlePlayingInput(game, event);
+                break;
+            case State::PAUSED:
+                handlePausedInput(game, event);
+                break;
+            case State::END_MENU:
+                handleEndMenuInput(game, event);
+                break;
+        }
+    }
+}
+
+
+void InputHandler::handleStartMenuInput(SnakeGame &game, const sf::Event &event) {
+    if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Space) {
+        game.start();
+    } else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Q) {
+        game.exitGame();
+    }
+}
+
+
+void InputHandler::handlePlayingInput(SnakeGame &game, const sf::Event &event) {
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) || sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+        game.changeDirection(Direction::UP);
+    } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) || sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+        game.changeDirection(Direction::LEFT);
+    } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) || sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+        game.changeDirection(Direction::DOWN);
+    } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) || sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+        game.changeDirection(Direction::RIGHT);
+    }
+
+    if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Space) {
+        game.pause();
+    }
+}
+
+
+void InputHandler::handlePausedInput(SnakeGame &game, const sf::Event &event) {
+    if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Space) {
+        game.resume();
+    } else if (event.key.code == sf::Keyboard::Q) {
+        game.reset();
+    }
+}
+
+
+void InputHandler::handleEndMenuInput(SnakeGame &game, const sf::Event &event) {
+    if (event.key.code == sf::Keyboard::Space) {
+        game.reset();
+    }
+}
--- src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "SnakeGame.h"
-
-int main() {
-    SnakeGame game(20, 20);
-    return 0;
-}
