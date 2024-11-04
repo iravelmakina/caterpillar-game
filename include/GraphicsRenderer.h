57e0d4d7486fac9589f391b09710b88043ba5aa5
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2024-11-04 17:23:48 +0200

    GraphicsRenderer class
    - implemented GraphicsRenderer class for handling logic connected with visuals and music with window, font, textures, cellSize, scoreBoardHeight fields
    - created methods renderGame(), renderStartMenu(), renderPauseMenu(), renderEndMenu() and helper methods drawCaterpillar(), drawApple(), drawWalls(), drawScoreBoard(), drawText(), loadTextures()

---
+++ include/GraphicsRenderer.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+
+class GraphicsRenderer {
+public:
+    GraphicsRenderer(sf::RenderWindow &window, unsigned int cellSize, unsigned int scoreBoardHeight);
+
+    void renderGame(const std::pair<unsigned int, unsigned int> &headPosition,
+                    const std::vector<std::pair<unsigned int, unsigned int> > &bodyPositions,
+                    const std::pair<unsigned int, unsigned int> &applePosition,
+                    const std::vector<std::pair<unsigned int, unsigned int> > &wallPositions,
+                    unsigned int currentScore, unsigned int bestScore) const;
+
+    void renderStartMenu() const;
+
+    void renderPauseMenu() const;
+
+    void renderEndMenu(unsigned int score, unsigned int bestScore, bool isWinner) const;
+
+private:
+    sf::RenderWindow &window;
+    sf::Font font;
+    sf::Texture wallTexture;
+    sf::Texture appleTexture;
+    sf::Texture caterpillarBodyTexture;
+    sf::Texture caterpillarHeadTexture;
+    unsigned int cellSize;
+    unsigned int scoreBoardHeight;
+
+
+    void drawCaterpillar(const std::pair<unsigned int, unsigned int> &headPosition,
+                   const std::vector<std::pair<unsigned int, unsigned int> > &
+                   bodyPositions) const;
+
+    void drawApple(const std::pair<unsigned int, unsigned int> &applePosition) const;
+
+    void drawWalls(const std::vector<std::pair<unsigned int, unsigned int> > &wallPositions) const;
+
+    void drawScoreBoard(unsigned int currentScore, unsigned int bestScore) const;
+
+    void drawText(const std::string &textStr, float x, float y, unsigned int fontSize) const;
+
+    void loadTextures();
+};
