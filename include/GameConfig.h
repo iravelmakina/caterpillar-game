0b0f78d49b8b891464362b8ced36d7517520d791
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2024-11-05 16:46:17 +0200

    GameConfig changes
    - added fps and windowTitle fields to define them before starting game

--- include/GameConfig.h
+++ include/GameConfig.h
@@ -1,8 +1,12 @@
 #pragma once
+#include <string>
+
 
 class GameConfig {
 public:
-    explicit GameConfig(unsigned int initialWindowWidth = 500, unsigned int initialCellSize = 20);
+    explicit GameConfig(unsigned int initialWindowWidth = 500, unsigned int initialCellSize = 20,
+                        unsigned int initialFPS = 60, const std::string &initialWindowTitle = "Caterpillar Game");
+
 
     unsigned int getWindowWidth() const;
 
@@ -14,10 +18,16 @@ public:
 
     unsigned int getScoreBoardHeight() const;
 
+    unsigned int getFPS() const;
+
+    const std::string &getWindowTitle() const;
+
 private:
     unsigned int windowWidth;
     unsigned int windowHeight;
     unsigned int cellSize;
     unsigned int gridSize;
     unsigned int scoreBoardHeight;
+    unsigned int fps;
+    std::string windowTitle;
 };
--- src/GameConfig.cpp
+++ src/GameConfig.cpp
@@ -2,14 +2,14 @@
 #include <cmath>
 
 
-GameConfig::GameConfig(const unsigned int initialWindowWidth, const unsigned int initialCellSize)
-    : windowWidth(initialWindowWidth), cellSize(initialCellSize) {
+GameConfig::GameConfig(const unsigned int initialWindowWidth, const unsigned int initialCellSize, const unsigned int initialFPS, const std::string &initialWindowTitle)
+    : windowWidth(initialWindowWidth), cellSize(initialCellSize), fps(initialFPS), windowTitle(initialWindowTitle) {
 
     gridSize = windowWidth / cellSize;
     windowWidth = gridSize * cellSize;
 
 
-    scoreBoardHeight = static_cast<unsigned int>(std::round(windowWidth * 0.12));
+    scoreBoardHeight = static_cast<unsigned int>(std::round(windowWidth * 0.1));
 
     windowHeight = windowWidth + scoreBoardHeight;
 }
@@ -36,4 +36,14 @@ unsigned int GameConfig::getGridSize() const {
 
 unsigned int GameConfig::getScoreBoardHeight() const {
     return scoreBoardHeight;
+}
+
+
+unsigned int GameConfig::getFPS() const {
+    return fps;
+}
+
+
+const std::string& GameConfig::getWindowTitle() const {
+    return windowTitle;
 }
